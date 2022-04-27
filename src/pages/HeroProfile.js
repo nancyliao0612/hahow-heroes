2944f1f6681c85a2cb04254a7e07be9448d7853a
Nancy <nancy62924@gmail.com>
2022-04-27 14:48:26 +0800

    add increasing and decreasing hero point function

--- src/pages/HeroProfile.js
+++ src/pages/HeroProfile.js
@@ -6,6 +6,7 @@ import styled from "styled-components";
 function HeroProfile() {
   const { heroId } = useParams();
   const [profile, setProfile] = useState("");
+  const [remainPoint, setRemainPoint] = useState(0);
 
   const url = `https://hahow-recruit.herokuapp.com/heroes/${heroId}/profile`;
 
@@ -25,33 +26,106 @@ function HeroProfile() {
   }, [heroId]);
 
   const { str, int, agi, luk } = profile;
+  let totalPoints = str + int + agi + luk;
+
+  // Increase single hero's point
+  function addPoints(e) {
+    let { name } = e.target;
+    let { point } = e.target.dataset;
+    if (remainPoint !== 0) {
+      setProfile((prevPoint) => {
+        return { ...prevPoint, [name]: Number(point) + 1 };
+      });
+      setRemainPoint((prevRemainPoint) => prevRemainPoint - 1);
+    }
+  }
+
+  // Decrease single hero's point
+  function minusPoints(e) {
+    let { name } = e.target;
+    let { point } = e.target.dataset;
+    if (point > 0) {
+      setProfile((prevPoint) => {
+        return { ...prevPoint, [name]: Number(point) - 1 };
+      });
+      setRemainPoint((prevRemainPoint) => prevRemainPoint + 1);
+    }
+  }
+  console.log(profile);
+
+  // count remain points
+  console.log(totalPoints);
 
   return (
     <Wrapper>
-      <div>
-        <span>STR</span>
-        <button>+</button>
-        <span>{str}</span>
-        <button>-</button>
-      </div>
-      <div>
-        <span>INT</span>
-        <button>+</button>
-        <span>{int}</span>
-        <button>-</button>
-      </div>
-      <div>
-        <span>AGI</span>
-        <button>+</button>
-        <span>{agi}</span>
-        <button>-</button>
-      </div>
-      <div>
-        <span>LUK</span>
-        <button>+</button>
-        <span>{luk}</span>
-        <button>-</button>
-      </div>
+      <form action="">
+        <div>
+          <span>STR</span>
+          <button type="button" onClick={addPoints} name="str" data-point={str}>
+            +
+          </button>
+          <span>{str}</span>
+          <button
+            type="button"
+            onClick={minusPoints}
+            name="str"
+            data-point={str}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <span>INT</span>
+          <button type="button" onClick={addPoints} name="int" data-point={int}>
+            +
+          </button>
+          <span>{int}</span>
+          <button
+            type="button"
+            onClick={minusPoints}
+            name="int"
+            data-point={int}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <span>AGI</span>
+          <button type="button" onClick={addPoints} name="agi" data-point={agi}>
+            +
+          </button>
+          <span>{agi}</span>
+          <button
+            type="button"
+            onClick={minusPoints}
+            name="agi"
+            data-point={agi}
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <span>LUK</span>
+          <button type="button" onClick={addPoints} name="luk" data-point={luk}>
+            +
+          </button>
+          <span>{luk}</span>
+          <button
+            type="button"
+            onClick={minusPoints}
+            name="luk"
+            data-point={luk}
+          >
+            -
+          </button>
+        </div>
+        <section>
+          <p>剩餘點數：{remainPoint}</p>
+          <button type="submit" className="submit-btn">
+            儲存
+          </button>
+        </section>
+      </form>
     </Wrapper>
   );
 }
@@ -62,6 +136,7 @@ const Wrapper = styled.section`
   padding: 2rem 3rem;
   font-size: 1.8rem;
   background-color: var(--clr-primary-3);
+  position: relative;
 
   div {
     margin-bottom: 2rem;
@@ -82,6 +157,24 @@ const Wrapper = styled.section`
     height: 5rem;
     width: 5rem;
     border: solid 1px;
+    cursor: pointer;
+  }
+
+  section {
+    position: absolute;
+    // border: solid 1px;
+    bottom: 3rem;
+    right: 3rem;
+  }
+
+  section p {
+    margin-bottom: 1rem;
+  }
+
+  section .submit-btn {
+    font-size: 2rem;
+    margin: 0;
+    width: 20rem;
   }
 `;
 
